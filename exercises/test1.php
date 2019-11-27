9f1450f618b4d8b99d79a9fd14c67f9dadcb79cd
Luigi Cardamone <luigi.cardamone@ped.technology>
2019-11-27 10:13:31 +0100

    Hide solution

--- exercises/test1.php
+++ exercises/test1.php
@@ -4,17 +4,4 @@
 // Memorizzare in un array i primi 10 multipli di 3.
 // Stampare a video tutti gli elementi dell’array e la somma totale.
 
-define('MAX_RESULTS', 10);
-define('NUM', 3);
 
-$array = [];
-
-for ($i = 0; $i < MAX_RESULTS; $i++) {
-    $array[] = ($i + 1) * NUM;
-}
-
-for ($i = 0; $i < count($array); $i++) {
-    echo $array[$i] . "<br>";
-}
-
-echo "Somma totale: " . array_sum($array);
